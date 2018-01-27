4a6229dbe2f64fef56fdaa27a86e08041ba7bc52
Rayman <Rayman@RAY>
2018-01-27 15:36:28 +0300

    1

---
+++ 111/src/lesson_1/Second.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package lesson_1;
+
+/**
+ * @author Rayman
+ *
+ */
+public class Second {
+
+	/**
+	 * @param args
+	 */
+	public void plus() {
+		// TODO Auto-generated method stub
+      
+	First a = new First();
+     a.setFirst(12);
+	}
+
+}
